5dbf5d3b24271b00bb5125948180046aaac2fbdf
Md H H Martin <engr.bmhhmartin@gmail.com>
2023-08-07 17:39:46 +0600

    code structure

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>About page</h1>
+            <img src="/images/stock-photo-sunset-in-patenga-sea-beach-2192537035.jpg" alt="sunset" />
+        </div>
+    );
+};
+
+export default page;
